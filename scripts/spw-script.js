a5fc2513e310fc4b2720c6fa7f2fd1667ccfe0df
Aaron Pruzaniec <aaronp@gwynniebee.com>
2017-09-06 01:52:15 -0400

    Finishing homepage

--- scripts/spw-script.js
+++ scripts/spw-script.js
@@ -53,13 +53,36 @@ balanceSides = function(){
         $('#header #mainNavigation').css({'padding-right':(SPW.nav.lrDiff)+'px'});
     }
 }
+calcDim = function(){
+    SPW.footer = {};
+    SPW.footer.topOffset = $('#footer').offset();
+    SPW.footer.bottomOffset = $('.footer-inner').offset();
+    SPW.footer.endorsementHeight = $('#endorsementsOuter').outerHeight();
+    SPW.footer.offset = SPW.footer.bottomOffset.top-SPW.footer.topOffset.top;
+    console.log(SPW);
+    // page height
+    $('.collection-type-index #page').css({
+        'height':(window.innerHeight-SPW.footer.offset-14-SPW.footer.endorsementHeight)+'px',
+        'position':'static'
+    });
+}
 tempInjectHTML = function(){
     $('<a href="http://crowdpac.com/c/spwayland" class="m-tb-1 sqs-block-button-element--small sqs-block-button-element" data-initialized="true">Contribute Now</a>').appendTo('.collection-type-index #page #content .sqs-block-content');
+    // index page content
+    $('.collection-type-index .sqs-layout.sqs-grid-12.columns-12 *').remove();
+    //index page endorsements
+    $('<div id="endorsementsOuter"><div class="fs-12 caps changaone c-secondary text-center">Endorsed by:</div><div id="endorsementsInner"><img src="../assets/vote-vets-logo-2x.jpg" alt="Vote Vets"></div></div>').insertAfter('.collection-58c5d2dabebafb01e0518dc1 #page');
+    //$('').appendTo('');
+    $('<div id="contentOuter"><img class="icon" src="../assets/Sean-Patrick-Wayland.jpg" alt="Sean Patrick Wayland"><div id="contentInner"><div id="slide1"><div class="changaone fs-26 caps ls-200 c-secondary">SEAN PATRICK WAYLAND<br>PHILADELPHIAN. FATHER. VETERAN.</div><div class="changaone ls-200 lh-2 c-secondary fs-40">CARRYING<br>YOUR VOICE<br>TO HARRISBURG.</div></div></div></div>').appendTo('.collection-58c5d2dabebafb01e0518dc1 .content-inner .sqs-layout');
 }
 $(document).ready(function(){
     builldNavigation();
     tempInjectHTML();
+    calcDim();
 });
 $(window).on('load',function(){
 
+});
+$(window).on('resize',function(){
+    calcDim();
 });
