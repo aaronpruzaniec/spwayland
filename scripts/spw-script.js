b789eb615abe5fb3bc1f856192cd302ca8d13713
aaronpruzaniec <aaronpruzaniec@gmail.com>
2018-08-15 13:20:04 -0400

    Project phase one complete

--- scripts/spw-script.js
+++ scripts/spw-script.js
@@ -31,7 +31,7 @@ openModal = function(){
         if($('#contributeOuter').length){
 
         }else{
-            $('<div id="contributeOuter" class="fs-32" style="width:'+SPW.modal.dimentions.width+'px;height:'+SPW.modal.dimentions.height+'px;top:'+SPW.modal.dimentions.offset.top+'px;left:'+SPW.modal.dimentions.offset.left+'px;background-color:#'+SPW.modal.dimentions.color+';position:fixed;z-index:1001;display:none;"><div id="openLinkOuter"><div id="openLinkInner"></div></div><div id="contributeClose" class="changaone">X</div><div id="contributeInner"><div class="outer"><div class="left"><div class="leftInner"><div class="changaone">Donate</div><div id="donateOuter"><div id="donateInner"><a href="https://www.crowdpac.com/contribute/193015?amount=3" class="donation changaone" data="3">$3</a><a href="https://www.crowdpac.com/contribute/193015?amount=10" class="donation changaone" data="10">$10</a><a href="https://www.crowdpac.com/contribute/193015?amount=15" class="donation changaone" data="15">$15</a><a href="https://www.crowdpac.com/contribute/193015" class="donation changaone" data="other">other</a></div></div></div></div><div class="right"><div class="rightInner"><div class="changaone">Newsletter</div><div id="emailInputOuter"><input id="emailInput" class="changaone fs-20" value="enter email address"></div></div></div></div></div></div>').insertBefore('body');
+            $('<div id="contributeOuter" class="fs-32" style="width:'+SPW.modal.dimentions.width+'px;height:'+SPW.modal.dimentions.height+'px;top:'+SPW.modal.dimentions.offset.top+'px;left:'+SPW.modal.dimentions.offset.left+'px;background-color:#'+SPW.modal.dimentions.color+';position:fixed;z-index:1001;display:none;"><div id="openLinkOuter"><div id="openLinkInner"></div></div><div id="contributeClose" class="changaone">X</div><div id="contributeInner"><div class="outer"><div class="left"><div class="leftInner"><div class="changaone">Donate</div><div id="donateOuter"><div id="donateInner"><a href="https://www.crowdpac.com/contribute/193015?amount=3" class="donation changaone" data="3">$3</a><a href="https://www.crowdpac.com/contribute/193015?amount=10" class="donation changaone" data="10">$10</a><a href="https://www.crowdpac.com/contribute/193015?amount=15" class="donation changaone" data="15">$15</a><a href="https://www.crowdpac.com/contribute/193015" class="donation changaone" data="other">other</a></div></div></div></div><div class="right"><div class="rightInner"><div class="changaone">Newsletter</div><div id="emailInputOuter"><form id="form1" name="form1" class="wufoo topLabel page table" accept-charset="UTF-8" autocomplete="off" enctype="multipart/form-data" method="post" novalidate action="https://aaronpruzaniec.wufoo.com/forms/zv730fq1slnc4v/#public"><div class="table" id="wufooOuter"><input id="emailInput" name="Field1" type="email" spellcheck="false" class="table-cell changaone element1" value="" maxlength="255" tabindex="1" placeholder="enter email" /><input id="saveForm" name="saveForm" class="btTxt submit table-cell changaone c-fff element2" type="submit" value="Submit" /><input type="hidden" id="idstamp" name="idstamp" value="1jrxh/GKidML6kkBlLKELMDWuPtSyCD39iakyH1P8jY=" /></div></form></div></div></div></div></div></div>').insertBefore('body');
         }
         // Build donation buttons once
         if(SPW.donations.donationsBuilt){
@@ -146,11 +146,30 @@ tempInjectHTML = function(){
     //$('').appendTo('');
     $('<div id="contentOuter" class="fs-40"><img class="icon" src="../assets/Sean-Patrick-Wayland.jpg" alt="Sean Patrick Wayland"><div id="contentInner"><div id="slide1"><div class="changaone fs-65-p caps ls-200 c-secondary">SEAN PATRICK WAYLAND<br>PHILADELPHIAN. FATHER. VETERAN.</div><div class="changaone ls-200 lh-2 c-secondary fs-100-p">CARRYING<br>YOUR VOICE<br>TO HARRISBURG.</div><div id="contributeContainer"><div class="external contributeButton"><a href="http://crowdpac.com/c/spwayland">Contribute</a></div></div></div></div></div>').appendTo('.collection-58c5d2dabebafb01e0518dc1 .content-inner .sqs-layout');
 }
+buildBackgrounds = function(){
+    var a = '<div class="background" style="background-image:url("/assets/hero1.jpg");"></div>';
+    var b;
+    var c = $('.collection-type-index').length;
+    SPW.backgrounds = [1,2,3,4];
+    var d = SPW.backgrounds.length;
+    if( c>0 ){
+        for(i=0;i<d;i++){
+            var e = setRandomNumber(d);
+            console.log(e);
+        }
+        console.log(c);
+    }
+
+}
+setRandomNumber = function(d){
+    return Math.floor((Math.random() * d) + 1);
+}
 $(document).ready(function(){
     builldNavigation();
     tempInjectHTML();
     calcDim();
     openModal();
+    buildBackgrounds();
 });
 $(window).on('load',function(){
 
